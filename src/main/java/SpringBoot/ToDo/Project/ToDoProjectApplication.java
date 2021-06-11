5ce268d378fa9b37e1c3a4d4cc8faeadd530db74
rawatravinder115 <rawatravinder115@gmail.com>
2021-06-11 15:06:48 +0530

    add member to todo list

--- src/main/java/SpringBoot/ToDo/Project/ToDoProjectApplication.java
+++ src/main/java/SpringBoot/ToDo/Project/ToDoProjectApplication.java
@@ -32,38 +32,47 @@ public class ToDoProjectApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 
+		todo t1 = new todo("javascript","cool laguage",System.currentTimeMillis(),System.currentTimeMillis(),"just do simple test");
+		todo t2= new todo("java","awesome thing",System.currentTimeMillis(),System.currentTimeMillis(),"simple test");
+
+
+
+
+//		tRepository.save(t1);
+//		tRepository.save(t2);
+
 		member m1 = new member("ravinder","ice");
 		member m2 = new member("yash","cs");
 		member m3 = new member("rishu","robotics");
+
+		m1.setTodo(t1);
+		m2.setTodo(t2);
+		m3.setTodo(t1);
+
+		t1.getMember().add(m1);
+		t1.getMember().add(m3);
+		t2.getMember().add(m2);
+
 
 //		mRepository.save(m1);
 //		mRepository.save(m2);
 //		mRepository.save(m3);
 
 
-
-		todo t1 = new todo("javascript","cool laguage",System.currentTimeMillis(),System.currentTimeMillis(),"just do simple test");
-		todo t2= new todo("java","awesome thing",System.currentTimeMillis(),System.currentTimeMillis(),"simple test");
-
-
-
-		tRepository.save(t1);
-		tRepository.save(t2);
-
 //		m1.setTodo(t1);
 //		m2.setTodo(t2);
 
-		t1.setMember(Arrays.asList(m1,m2,m3));
+//		t1.setMember(Arrays.asList(m1,m2,m3));
 //		t1.setMember(new member(m2));
 //		t1.setMember(m3);
 //
-		t2.setMember(Arrays.asList(m2,m3));
+//		t2.setMember(Arrays.asList(m2,m3));
 //		t2.setMember(m2);
 //		t2.setMember(m3);
 //
-		mRepository.save(m1);
-		mRepository.save(m2);
-		mRepository.save(m3);
+//		mRepository.save(m1);
+//		mRepository.save(m2);
+//		mRepository.save(m3);
 		tRepository.save(t1);
 		tRepository.save(t2);
 
